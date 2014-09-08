740535ecf540f42ca205210966c675b5dc396124
richardm <richard.mason@eastsussex.gov.uk>
2014-09-08 17:37:43 +0100

    Prevent unnecessary load of Twitter script

--- js/social-media.js
+++ js/social-media.js
@@ -191,13 +191,15 @@ window.fbAsyncInit = function ()
 */
 (function ()
 {
-    var twitterWidgets = document.createElement('script');
-    twitterWidgets.type = 'text/javascript';
-    twitterWidgets.async = true;
-    twitterWidgets.src = '//platform.twitter.com/widgets.js';
+    if (typeof (jQuery) != 'undefined' && $(".twitter-share-button").length > 0) {
+        var twitterWidgets = document.createElement('script');
+        twitterWidgets.type = 'text/javascript';
+        twitterWidgets.async = true;
+        twitterWidgets.src = '//platform.twitter.com/widgets.js';
 
-    // Setup a callback to track once the script loads.
-    twitterWidgets.onload = _ga.trackTwitter;
+        // Setup a callback to track once the script loads.
+        twitterWidgets.onload = _ga.trackTwitter;
 
-    document.getElementsByTagName('head')[0].appendChild(twitterWidgets);
+        document.getElementsByTagName('head')[0].appendChild(twitterWidgets);
+    }
 })();
