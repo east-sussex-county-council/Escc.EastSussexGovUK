a77cdf9421951402382aefae1195cd05c38fdf0c
richardm <richard.mason@eastsussex.gov.uk>
2014-09-17 16:53:59 +0100

    Add base view model for MVC

---
+++ BaseViewModel.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Web;
+
+namespace EsccWebTeam.EastSussexGovUK
+{
+    /// <summary>
+    /// Base class for common properties to be available to all view models used on www.eastsussex.gov.uk
+    /// </summary>
+    public abstract class BaseViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseViewModel"/> class.
+        /// </summary>
+        protected BaseViewModel()
+        {
+            IsPublicView = true;
+        }
+        /// <summary>
+        /// Gets or sets the page title
+        /// </summary>
+        public string PageTitle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the JavaScript for a Google Analytics content experiment
+        /// </summary>
+        public IHtmlString ContentExperimentScript { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether the current view is a publicly-visible page
+        /// </summary>
+        public bool IsPublicView { get; set; }
+    }
+}
