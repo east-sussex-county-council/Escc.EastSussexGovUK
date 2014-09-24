d5670ed52efa93671a3f0ae9bbaacafb2d57e111
richardm <richard.mason@eastsussex.gov.uk>
2014-09-24 14:55:39 +0100

    Add config setting for loading CSS files

--- js/config.example.js
+++ js/config.example.js
@@ -1,5 +1,6 @@
 ﻿var esccConfig = {
     GoogleMapsApiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
     GoogleAnalyticsPublicWebsite: 'UA-xxxxxxxxx-x',
-    AlertsUrl: 'https://hostname/path/to/alerts.json'
+    AlertsUrl: 'https://hostname/path/to/alerts.json',
+    CssHandlerPath: 'https://hostname/path/to/css/{0}.cssx'
 }
