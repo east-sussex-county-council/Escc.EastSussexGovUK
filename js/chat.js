51e1d9dc3ecd0be4c29ce4023a2996607bec2ed2
richardm <richard.mason@eastsussex.gov.uk>
2014-10-15 17:17:11 +0100

    Add web chat

---
+++ js/chat.js
@@ -0,0 +1,102 @@
+﻿// The icon used for our web chat header is by Freepik (www.freepik.com) from www.flaticon.com, and licensed under CC BY 3.0.
+
+// Use a self-executing function, but run it now. If we wait for an event like DOM ready the document will be closed,
+// and any attempt to use document.write will wipe out all the content. LogoNet code uses document.write so we're forced to.
+(function () {
+    var chatInclude = [
+        '/contactus/',
+        '/roadsandtransport/roads/default.htm',
+        '/roadsandtransport/roads/roadworks/',
+        '/roadsandtransport/roads/maintenance/', 
+        '/roadsandtransport/roads/contactus/',
+        '/roadsandtransport/roads/roadadoption/',
+        '/roadsandtransport/roads/roadsafety/',
+        '/roadsandtransport/roads/maintenance/fixtures/',
+        '/roadsandtransport/bexhillhastingslinkroad/',
+        '/roadsandtransport/roads/roadschemes/uckfieldtransport/',
+        '/community/emergencyplanningandcommunitysafety/emergencyplanning/advice/flooding/',
+        '/leisureandtourism/countryside/rightsofway/',
+        '/environment/rubbishandrecycling/recyclingsites/',
+        '/environment/woodlands/dutchelms/',
+        '/socialcare/aboutus/contact.htm',
+        '/socialcare/aboutus/complaints/default.htm',
+        '/socialcare/gettinghelp/default.htm',
+        '/socialcare/gettinghelp/apply/default.htm',
+        '/socialcare/gettinghelp/eligibility/default.htm',
+        '/socialcare/servicerequestforms/introduction.aspx',
+        '/socialcare/disability/learning/contacts/socialcaredirect.htm',
+        '/socialcare/athome/approvedproviders/'
+    ];
+
+    var chatExclude = [
+        '/roadsandtransport/roads/licences/',
+        '/roadsandtransport/roads/largevehicles/',
+        '/roadsandtransport/roads/roadschemes/cycleway.htm',
+        '/roadsandtransport/roads/roadschemes/rail/',
+        '/roadsandtransport/roads/roadschemes/a259corridor/',
+        '/roadsandtransport/public/',
+        '/roadsandtransport/parking/',
+        '/educationandlearning/schools/transport/',
+        '/roadsandtransport/planning/',
+        '/contactus/apply/default.htm',
+        '/contactus/pay/default.htm',
+        '/contactus/pay/about.htm',
+        '/contactus/languages.htm',
+        '/contactus/interpreting.htm',
+        '/socialcare/gettinghelp/advocacy.htm',
+        '/socialcare/gettinghelp/money/',
+        '/socialcare/worried/report/default.htm'
+    ];
+
+    var found = function(paths) {
+        var len = paths.length;
+        for (var i = 0; i < len; i++) {
+            if (location.pathname.indexOf(paths[i]) === 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    var includeScript = function() {
+        // LogoNet code for including web chat
+        var script = "https://eastsussex.logo-net.co.uk/Delivery/DBURLssl.php";
+        var page = parent.document.URL;
+        page = page.replace(/&/g, "^");
+        page = page.toLowerCase();
+        page = page.replace(/</g, "-1");
+        page = page.replace(/>/g, "-2");
+        page = page.replace(/%3c/g, "-1");
+        page = page.replace(/%3e/g, "-2");
+        document.write("<scr" + "ipt src='" + script);
+        document.write("?SDTID=192");
+        document.write('&PURL=' + page);
+        document.write('&CMS=' + new Date().getTime());
+        document.write("'></scr" + "ipt>");
+    }
+
+    var insertDiv = function () {
+        // Aim for top of the right column. Look first for a single .article and insert after it.
+        // If that fails look for any right-col content, and insert before it.
+        var elements = $(".article");
+        var chat = $('<div class="supporting large" id="webchat" />')[0];
+        if (elements.length === 1) {
+            elements[0].parentNode.insertBefore(chat, elements[0].nextSibling);
+            return true;
+        } else {
+            elements = $(".supporting,.supporting-text");
+            if (elements.length) {
+                elements[0].parentNode.insertBefore(chat, elements[0]);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Decide whether to load resources from LogoNet
+    if (found(chatInclude) && !found(chatExclude)) {
+        if (insertDiv()) {
+            includeScript();
+        }
+    };
+})();
