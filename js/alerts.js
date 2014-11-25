04863b956d5f05fe34f77cf4d37f20341e6603a6
richardm <richard.mason@eastsussex.gov.uk>
2014-11-25 16:36:11 +0000

    Add Vary parameter to work with CORS protection

--- js/alerts.js
+++ js/alerts.js
@@ -6,7 +6,9 @@
         // Use code from https://github.com/johnkpaul/jquery-ajax-retry to mitigate against network errors, which are the main
         // cause of no JavaScript according to gov.uk research https://gds.blog.gov.uk/2013/10/21/how-many-people-are-missing-out-on-javascript-enhancement/
         // 
-        $.ajax({ dataType: "json", url: esccConfig.AlertsUrl }).retry({ times: 3 }).then(function(data) {
+        // Add vary={origin} to querystring as setting Vary header from view is getting overridden. Need to vary the response by origin somehow, otherwise it requests the alerts
+        // from Origin A, caches the response with a CORS header allowing Origin A, then requests the alerts from Origin B, gets the cached version and fails the CORS test.
+        $.ajax({ dataType: "json", url: esccConfig.AlertsUrl + "?vary=" + document.location.protocol + "//" + document.location.host }).retry({ times: 3 }).then(function (data) {
 
             // Note: Must filter inheritance before cascade. If an ancestor of the current page blocks inheritance it needs to be
             // considered when deciding how far up the tree to inherit, before it potentially gets removed if it blocks cascade as well.
