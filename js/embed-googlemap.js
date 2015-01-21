d3ffbfa00ca8e0599fcdf55bb5f172149453ee93
Rick Mason <richard.mason@eastsussex.gov.uk>
2015-01-21 17:54:28 +0000

    Add generic embed scripts for YouTube and Google maps links

---
+++ js/embed-googlemap.js
@@ -0,0 +1,35 @@
+﻿if (jQuery != 'undefined' && esccGoogleMaps != 'undefined') {
+    $(function () {
+        esccGoogleMaps.loadGoogleMapsApi({ callback: "esccEmbedLinkedGoogleMap" });
+    });
+}
+
+function esccEmbedLinkedGoogleMap() {
+    $("a")
+    .filter(function () { return /maps\.google\.co\.uk\/maps\/ms\?msid=[0-9a-f.]+&msa=0/.test(this.href); })
+    .each(function () {
+        // Note: frameborder attribute is deprecated, but still needed by IE8
+        var mapUrl = this.href + '&ie=UTF8&t=m&output=embed';
+        if (!this.href.match(/&z=[0-9]/)) {
+            mapUrl += "&z=14";
+        }
+        var iframe = $('<div class="googlemap"><iframe frameborder="0" width="100%" height="400" seamless="seamless" title="Map" src="' + mapUrl + '"></iframe><p><a href="' + mapUrl + '">View a larger map</a></p></div>')[0];
+        replaceWithMap(this, iframe);
+    });
+
+    function replaceWithMap(replaceThis, withThis) {
+        // Has this link got any siblings other than white space?
+        var hasSiblings = false;
+        var siblings = replaceThis.parentNode.childNodes;
+        var len = siblings.length;
+        for (var i = 0; i < len; i++) {
+            if (siblings[i] == replaceThis) continue;
+            if (/[^\t\n\r ]/.test(siblings[i].data)) hasSiblings = true;
+        }
+
+        // Replace the original link, and its container if empty, with the map
+        var replace = hasSiblings ? replaceThis : replaceThis.parentNode;
+        $(withThis).insertBefore(replace);
+        $(replace).remove();
+    }
+}
---
+++ js/embed-youtube.js
@@ -0,0 +1,35 @@
+﻿if (typeof (jQuery) != 'undefined') {
+    $(function() {
+        
+        var youTube = new RegExp(/^https?:\/\/(youtu.be\/|www.youtube.com\/watch\?v=)([A-Za-z0-9_-]+)$/);
+        var youTubeWidth = 450, youTubeHeight = 318, youTubeResize = false;
+
+        function embedYouTube() {
+            $("a.embed").filter(function(index) {
+                return youTube.test(this.href);
+            }).each(function () {
+
+                if ($(this.parentNode).width() >= youTubeWidth) {
+
+                    // Swop YouTube link for embedded video
+                    var match = youTube.exec(this.href);
+                    $(this).replaceWith('<iframe width="' + youTubeWidth + '" height="' + youTubeHeight + '" src="https://www.youtube-nocookie.com/embed/' + match[2] + '" frameborder="0" allowfullscreen="allowfullscreen" class="video"></iframe>');
+
+                    // Once the window's big enough, stop watching resize
+                    if (youTubeResize) {
+                        $(window).off("resize", embedYouTube);
+                        youTubeResize = false;
+                    }
+                } else {
+                    // If the window isn't big enough, watch for resize
+                    if (!youTubeResize) {
+                        $(window).on("resize", embedYouTube);
+                        youTubeResize = true;
+                    }
+                }
+            });            
+        };
+
+        embedYouTube();
+    });
+}
