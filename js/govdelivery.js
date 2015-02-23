104179b8f6d8efd591f8881cf4932822b8b2f07b
Rick Mason <richard.mason@eastsussex.gov.uk>
2015-02-23 15:27:04 +0000

    First draft

---
+++ js/govdelivery.js
@@ -0,0 +1,17 @@
+﻿$(function() {
+    $("<a href=\"#\" class=\"govdelivery\">Keep me posted</a>").appendTo(".header .container").click(function(e) {
+        e.preventDefault();
+
+        if (!document.getElementById('govdelivery')) {
+            $('<link>').appendTo('head').attr({ type: 'text/css', rel: 'stylesheet', 'href': '/css/govdelivery.css?refresh=' + Date.now() });
+            $.get("/masterpages/controls/govdelivery.html", function(data) {
+                $(data).hide().insertBefore(".header").slideDown();
+            });
+        } else {
+            $("#govdelivery").slideToggle();
+        }
+
+       
+    });
+
+});
