0b4040f5393c3edbd63fb2045a13e651f01e80a1
Rick Mason <richard.mason@eastsussex.gov.uk>
2015-09-02 16:39:29 +0100

    Change web chat provider for Umbraco pages

--- Escc.EastSussexGovUK/js/webchat.js
+++ Escc.EastSussexGovUK/js/webchat.js
@@ -1,47 +1,40 @@
-﻿// The icon used for our web chat header is by Freepik (www.freepik.com) from www.flaticon.com, and licensed under CC BY 3.0.
+﻿// This version of the script is used on new.eastsussex.gov.uk, and will ultimately replace chat.js
 
-// Use a self-executing function, but run it now. If we wait for an event like DOM ready the document will be closed,
-// and any attempt to use document.write will wipe out all the content. LogoNet code uses document.write so we're forced to.
+// This function needs to be in the global namespace because its a callback used by the basic Click4Assistance window.
+function C4AWJSLoaded() {
+    oC4AW_Widget = new oC4AW_Widget();
+    oC4AW_Widget.setAccGUID("0f062fe2-f1c1-4727-9009-678f13d3a9e8");
+    oC4AW_Widget.setWSGUID("f96394e1-d493-4cb7-852d-c725a30de8fd");
+    oC4AW_Widget.setWFGUID("38424a83-823d-48b2-b013-63275f6ffe4e");
+    oC4AW_Widget.setPopupWindowWFGUID("59976160-4b48-44d7-b135-36207f209a5a");
+    oC4AW_Widget.setDockPosition("BOTTOM");
+    oC4AW_Widget.setBtnStyle("position:fixed; border:none; bottom:0em; right:1em;");
+    oC4AW_Widget.setIdentity("Embedded Chat");
+    oC4AW_Widget.Initilize();
+}
 
-// This version of the script is used on new.eastsussex.gov.uk, and will ultimately replace chat.js
-(function () {
-    var includeScript = function() {
-        // LogoNet code for including web chat
-        var script = "https://eastsussex.logo-net.co.uk/Delivery/DBURLssl.php";
-        var page = parent.document.URL;
-        page = page.replace(/&/g, "^");
-        page = page.toLowerCase();
-        page = page.replace(/</g, "-1");
-        page = page.replace(/>/g, "-2");
-        page = page.replace(/%3c/g, "-1");
-        page = page.replace(/%3e/g, "-2");
-        document.write("<scr" + "ipt src='" + script);
-        document.write("?SDTID=192");
-        document.write('&PURL=' + page);
-        document.write('&CMS=' + new Date().getTime());
-        document.write("'></scr" + "ipt>");
-    }
+// This function needs to be in the global namespace because its a callback used by the Click4Assistance tracking/proactive feature
+var C4A_TB;
+function C4AJSJustLoaded() {
+    C4A_TB = C4ATB();
+    C4A_TB.setAccountGUID('0f062fe2-f1c1-4727-9009-678f13d3a9e8');
+    C4A_TB.setWebsiteGUID('f96394e1-d493-4cb7-852d-c725a30de8fd');
+    C4A_TB.setUseCookie(true);
+    C4A_TB.Run();
+}
 
-    var insertDiv = function () {
-        // Aim for top of the right column. Look first for a single .article and insert after it.
-        // If that fails look for any right-col content, and insert before it.
-        var elements = $(".article");
-        var chat = $('<div class="supporting large" id="webchat" />')[0];
-        if (elements.length === 1) {
-            elements[0].parentNode.insertBefore(chat, elements[0].nextSibling);
-            return true;
-        } else {
-            elements = $(".supporting,.supporting-text");
-            if (elements.length) {
-                elements[0].parentNode.insertBefore(chat, elements[0]);
-                return true;
-            }
+$(function () {
+    function load(url) {
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.charset = 'utf-8';
+        script.defer = true;
+        script.src = url;
+        if (head) {
+            head.appendChild(script);
         }
-        return false;
     }
 
-    // Decide whether to load resources from LogoNet
-    if (insertDiv()) {
-        includeScript();
-    }
-})();
+    load('https://prod3si.click4assistance.co.uk/JS/ChatWidget.js');
+    load('https://prod3si.click4assistance.co.uk/JS/TrackProactive.js');
+});
