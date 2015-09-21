8fab2adf8f722d644f0e3f29148b71b2ac1476df
Rick Mason <richard.mason@eastsussex.gov.uk>
2015-09-21 12:19:31 +0100

    Add support for ESCIS search widget

---
+++ Escc.EastSussexGovUK/MasterPages/Features/EscisSearch.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+namespace Escc.EastSussexGovUK.MasterPages.Features
+{
+    /// <summary>
+    /// A search box for the ESCIS website
+    /// </summary>
+    public class EscisSearch : IClientDependencySet
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether to show ESCIS search.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> to show search; otherwise, <c>false</c>.
+        /// </value>
+        public bool ShowEscisSearch { get; set; }
+
+        /// <summary>
+        /// Determines whether the dependency is required
+        /// </summary>
+        /// <returns>
+        ///   <c>true</c> if the dependency is required; <c>false</c> otherwise
+        /// </returns>
+        public bool IsRequired()
+        {
+            return ShowEscisSearch;
+        }
+
+        /// <summary>
+        /// The CSS which is required for the dependent feature. These are registered in web.config using <see cref="Escc.ClientDependencyFramework" />.
+        /// </summary>
+        /// <returns>
+        /// A set of CSS file aliases, optionally qualified by media query aliases
+        /// </returns>
+        public IEnumerable<CssFileDependency> RequiresCss()
+        {
+            return new CssFileDependency[0];
+        }
+
+        /// <summary>
+        /// The JavaScript which is required for the dependent feature. These are registered in web.config using <see cref="Escc.ClientDependencyFramework" />.
+        /// </summary>
+        /// <returns>
+        /// A set of JS file aliases, qualified by a priority value which defaults to 100
+        /// </returns>
+        public IEnumerable<JsFileDependency> RequiresJavaScript()
+        {
+            return new JsFileDependency[1] { new JsFileDependency() { JsFileAlias = "Escis" } };
+
+        }
+
+        /// <summary>
+        /// The content security policy aliases required for the dependent feature. These are registered in web.config using <see cref="EsccWebTeam.Data.Web.ContentSecurityPolicy" />.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<ContentSecurityPolicyDependency> RequiresContentSecurityPolicy()
+        {
+            return new ContentSecurityPolicyDependency[1]
+            {
+                new ContentSecurityPolicyDependency(){ Alias = "Escis" }
+            };
+        }
+    }
+}
---
+++ Escc.EastSussexGovUK/js/escis.js
@@ -0,0 +1,17 @@
+﻿if (typeof (jQuery) !== 'undefined') {
+    $(function() {
+        /* If <div class="escis" /> is in the page, load an iframe inside it. But only load the iframe if we're above the large breakpoint (or if that test is not supported). */
+        var escisLoaded = false;
+        function loadEscis() {
+            if (escisLoaded) return;
+
+            if (!window.matchMedia || window.matchMedia("(min-width: 802px)").matches) {
+                $(".escis").append('<iframe src="https://www.escis.org.uk/escis-widget/escis-widget.html" frameborder="0" width="100%" height="459px" />');
+                escisLoaded = true;
+            }
+        }
+
+        $(window).resize(loadEscis);
+        loadEscis();
+    });
+}
