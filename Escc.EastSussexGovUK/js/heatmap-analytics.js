69544258e4942b18deb5f9d98ba9f0ec0a24ba4a
Rick Mason <richard.mason@eastsussex.gov.uk>
2015-09-23 10:39:17 +0100

    Change Crazy Egg account details

--- Escc.EastSussexGovUK/js/heatmap-analytics.js
+++ Escc.EastSussexGovUK/js/heatmap-analytics.js
@@ -50,7 +50,7 @@
             setTimeout(function () {
                 var a = document.createElement("script");
                 var b = document.getElementsByTagName("script")[0];
-                a.src = document.location.protocol + "//script.crazyegg.com/pages/scripts/0035/1997.js?" + Math.floor(new Date().getTime() / 3600000);
+                a.src = document.location.protocol + "//script.crazyegg.com/pages/scripts/0033/4885.js?" + Math.floor(new Date().getTime() / 3600000);
                 a.async = true; a.type = "text/javascript";
                 b.parentNode.insertBefore(a, b);
             }, 1);
