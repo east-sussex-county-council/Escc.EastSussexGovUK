d5f75d5462ab171f537f0d46232eb881e66994cb
Rick Mason <richard.mason@eastsussex.gov.uk>
2015-12-22 10:53:19 +0000

    Move guide nav into shared project

---
+++ Escc.EastSussexGovUK/js/nav.js
@@ -0,0 +1,18 @@
+﻿if (typeof (jQuery) != 'undefined') {
+    $(function() {
+        // Show/hide the navigation within the guide. CSS ensures this only takes effect at mobile sizes.
+        var menu = $(".guide-nav").hide();
+        var toggle = $('<div class="menu-toggle menu-toggle-show small">Show all pages in this section</div>');
+
+        toggle.insertBefore(menu).click(function () {
+            if (menu.is(":visible")) {
+                menu.slideUp();
+                toggle.removeClass("menu-toggle-hide").addClass("menu-toggle-show").text("Show all pages in this section");
+            } else {
+                menu.slideDown();
+                toggle.removeClass("menu-toggle-show").addClass("menu-toggle-hide").text("Hide all pages in this section");
+            }
+        });
+
+    });
+}
