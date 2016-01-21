73247acfddbaaf0dbfc2f659c751703d8de1c78e
Rick Mason <richard.mason@eastsussex.gov.uk>
2016-01-21 18:04:42 +0000

    ESCIS widget has been revised, so the frame can be smaller

--- Escc.EastSussexGovUK/js/escis.js
+++ Escc.EastSussexGovUK/js/escis.js
@@ -6,7 +6,7 @@
             if (escisLoaded) return;
 
             if (!window.matchMedia || window.matchMedia("(min-width: 802px)").matches) {
-                $(".escis").append('<iframe src="https://www.escis.org.uk/escis-widget/escis-widget.html" frameborder="0" width="100%" height="459px" />');
+                $(".escis").append('<iframe src="https://www.escis.org.uk/escis-widget/escis-widget.html" frameborder="0" width="100%" height="400px" />');
                 escisLoaded = true;
             }
         }
