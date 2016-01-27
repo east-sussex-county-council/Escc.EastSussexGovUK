933ce6fbfb0764d20c1ebad724c4329ac7c2ff3c
Rick Mason <richard.mason@eastsussex.gov.uk>
2016-01-27 11:15:21 +0000

    Add option for .menu-toggle to extend to medium sizes

--- Escc.EastSussexGovUK/js/nav.js
+++ Escc.EastSussexGovUK/js/nav.js
@@ -1,8 +1,12 @@
 ﻿if (typeof (jQuery) != 'undefined') {
     $(function() {
         // Show/hide the navigation within the guide. CSS ensures this only takes effect at mobile sizes.
-        var menu = $(".guide-nav").hide();
+        var menu = $(".guide-nav, .menu-nav, .menu-nav-medium").hide();
         var toggle = $('<div class="menu-toggle menu-toggle-show small">Show all pages in this section</div>');
+        if (menu.hasClass("menu-nav-medium"))
+        {
+            toggle.addClass("menu-toggle-medium");
+        }
 
         toggle.insertBefore(menu).click(function () {
             if (menu.is(":visible")) {
