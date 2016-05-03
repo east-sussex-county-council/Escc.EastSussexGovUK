951d67933603fb812bd259ddb0eef155921f7be7
Rick Mason <richard.mason@eastsussex.gov.uk>
2016-05-03 17:03:09 +0100

    Reassign web chat from highways contact centre to Social Care Direct

--- Escc.EastSussexGovUK/js/webchat.js
+++ Escc.EastSussexGovUK/js/webchat.js
@@ -3,8 +3,8 @@ function C4AWJSLoaded() {
     oC4AW_Widget = new oC4AW_Widget();
     oC4AW_Widget.setAccGUID("0f062fe2-f1c1-4727-9009-678f13d3a9e8");
     oC4AW_Widget.setWSGUID("f96394e1-d493-4cb7-852d-c725a30de8fd");
-    oC4AW_Widget.setWFGUID("38424a83-823d-48b2-b013-63275f6ffe4e");
-    oC4AW_Widget.setPopupWindowWFGUID("59976160-4b48-44d7-b135-36207f209a5a");
+    oC4AW_Widget.setWFGUID("71eb7c15-e400-49ea-a39f-dbae04be43d4"); // ID for Social Care Direct
+    oC4AW_Widget.setPopupWindowWFGUID("3afcea86-e223-4179-9985-1d548133484c"); // ID for Social Care Direct
     oC4AW_Widget.setDockPosition("BOTTOM");
     oC4AW_Widget.setBtnStyle("position:fixed; border:none; bottom:0em; right:1em;");
     oC4AW_Widget.setIdentity("Embedded Chat");
