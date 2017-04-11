98d550fe229ad2ae262e3aff43e30ceace33c0b7
Rick Mason <richard.mason@eastsussex.gov.uk>
2017-04-11 16:44:13 +0100

    Update deployment script to reflect recent changes

--- app-install-website.ps1
+++ app-install-website.ps1
@@ -67,7 +67,7 @@ $transformsFolder = NormaliseFolderPath $transformsFolder
 
 BackupApplication "$destinationFolder\$projectName" $backupFolder $comment
     
-robocopy $sourceFolder "$destinationFolder\$projectName" /S /PURGE /IF *.dll *.ico *.css *.js apple-*.png navigation.png desktop.png pan-*.gif item-type.png default.aspx logo-large.gif google*.html csc.* csi.* /XD aspnet_client obj Properties "Web References"
+robocopy $sourceFolder "$destinationFolder\$projectName" /S /PURGE /IF *.dll *.ico *.css *.js apple-*.png navigation.png desktop.png pan-*.gif item-type.png social.png default.aspx logo-large.gif google*.html csc.* csi.* /XD aspnet_client obj Properties "Web References"
 if (!(Test-Path "$destinationFolder\$projectName\views")) {
 	md "$destinationFolder\$projectName\views"
 }
