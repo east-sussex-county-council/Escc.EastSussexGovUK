20d9a5fd3c94d1735a5cf700211f48570661ad56
Rick Mason <richard.mason@eastsussex.gov.uk>
2017-12-12 17:43:43 +0000

    Update Twitter widget content security policy, and remove the NuBuild dependency for that NuGet package

---
+++ Escc.EastSussexGovUK.NuGet/build-nuget.cmd
@@ -0,0 +1,6 @@
+﻿@echo off
+set nuspec="%1"
+set nuspec=%nuspec:\=\\%
+nuget pack "%nuspec%Escc.EastSussexGovUK.nuspec"
+nuget pack "%nuspec%ClientDependency\Escc.EastSussexGovUK.ClientDependency.nuspec"
+nuget pack "%nuspec%SecurityConfig\Escc.EastSussexGovUK.SecurityConfig.nuspec"
--- Escc.EastSussexGovUK.TemplateSource/NuGet/ClientDependency/build-nuget.cmd
+++
@@ -1,4 +0,0 @@
-﻿@echo off
-set nuspec="%1"
-set nuspec=%nuspec:\=\\%
-nuget pack "%nuspec%Escc.EastSussexGovUK.ClientDependency.nuspec"
