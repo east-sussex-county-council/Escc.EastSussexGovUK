59f9737be8bcbb59affe0a838a8bbb00a4a160a6
Rick Mason <richard.mason@eastsussex.gov.uk>
2018-04-06 17:12:48 +0100

    Use modern CSS rather than floats for .guide-nav columns, and add support for bulleted .guide-nav. Prevent printing of the .guide-nav toggle control.

--- Escc.EastSussexGovUK.TemplateSource/js/nav.js
+++ Escc.EastSussexGovUK.TemplateSource/js/nav.js
@@ -2,7 +2,7 @@
     $(function() {
         // Show/hide the navigation within the guide. CSS ensures this only takes effect at mobile sizes.
         var menu = $(".guide-nav, .menu-nav, .menu-nav-medium").hide();
-        var toggle = $('<div class="menu-toggle menu-toggle-show small">Show all pages in this section</div>');
+        var toggle = $('<div class="menu-toggle menu-toggle-show small screen">Show all pages in this section</div>');
         if (menu.hasClass("menu-nav-medium"))
         {
             toggle.addClass("menu-toggle-medium");
